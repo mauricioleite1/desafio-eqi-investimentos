acb2910b09331c5549b5a953cb86717fc7d40afc
Maurício Leite <mauricioleite1@gmail.com>
2022-02-03 20:51:27 -0300

    Início de Layout e Testes da Tela Inicial

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { cleanup, render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
-describe('Home', () => {
-  it('renders a heading', () => {
+describe('Página Inicial', () => {
+  afterEach(cleanup)
+
+  it('1 . Renderiza o título da página corretamente', () => {
     render(<Home />)
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/ig,
-    })
+    const heading = screen.getByRole('heading', { name: 'Simulador de Investimentos' });
 
     expect(heading).toBeInTheDocument()
   })
+
+  it('2 . Renderiza dois botões da seção Rendimento', () => {
+    render(<Home />)
+
+    const buttons = screen.getAllByRole('button');
+    expect(buttons.length).toBe(2);
+  });
 })
