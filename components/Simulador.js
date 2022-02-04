df4f29b758fe606431eb0cede7f077bbf566f81e
Maurício Leite <mauricioleite1@gmail.com>
2022-02-04 11:21:05 -0300

    Estrutura dos Forms concluídos

--- components/Simulador.js
+++ components/Simulador.js
@@ -3,8 +3,8 @@ import styles from '../styles/Simulador.module.scss';
 
 const Simulacao = () => {
   return (
+      // <h2>Simulador</h2>
     <div className={styles.container}>
-      <h2>Simulador</h2>
 
       <form>
         <div className={styles.choice}>
@@ -36,6 +36,40 @@ const Simulacao = () => {
             <h4>IPCA (ao ano)</h4>
           </label>
           <input type='text' id='ipca' />
+        </div>
+      </form>
+
+      <form>
+        <div className={styles.choice}>
+          <div>
+            <h4>TIpos de indexação</h4>
+            <span>
+              <h4>i</h4>
+            </span>
+          </div>
+
+          <div>
+            <button className='btn btn-choice'>PRÉ</button>
+            <button className='btn btn-choice'>PÓS</button>
+            <button className='btn btn-choice'>FIXADO</button>
+          </div>
+        </div>
+
+        <div className={styles.inputsContainer}>
+          <label htmlFor='aporte-mensal'>
+            <h4>Aporte Mensal</h4>
+          </label>
+          <input type='text' id='aporte-mensal' />
+
+          <label htmlFor='rentabilidade'>
+            <h4>Rentabilidade</h4>
+          </label>
+          <input type='text' id='rentabilidade' />
+
+          <label htmlFor='cdi'>
+            <h4>CDI (ao ano)</h4>
+          </label>
+          <input type='text' id='cdi' />
         </div>
       </form>
     </div>
